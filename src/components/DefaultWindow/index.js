2c5dd8036104026307bb484cef40131957c4e373
Rajat Verma <rajatverma5885045@gmail.com>
2020-05-28 17:02:55 +0530

    fix height overflow editor

--- src/components/DefaultWindow/index.js
+++ src/components/DefaultWindow/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DefaultWindow = () => {
   return (
     <div className="default-window">
-      <h1>Start by writing some HTML code</h1>
+      <p>Start by writing some HTML code</p>
     </div>
   );
 };
