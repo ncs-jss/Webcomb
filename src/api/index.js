66c4d13fbe37a6dfa63c6fef84911aa84f25754d
Rajat Verma <rajatverma5885045@gmail.com>
2021-06-03 01:41:47 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
-
 export const uploadImage = async (formData) => {
   try {
     const token = process.env.REACT_APP_IMGBB_API_KEY;
-    const url = `${corsAnywhere}${process.env.REACT_APP_IMAGEBB_URL}?key=${token}`;
+    const url = `${process.env.REACT_APP_IMAGEBB_URL}?key=${token}`;
     const options = {
       headers: {
         'content-type': 'multipart/form-data',
